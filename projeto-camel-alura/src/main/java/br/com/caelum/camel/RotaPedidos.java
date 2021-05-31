ec8e4909784e788a9f93eadc508640d93a4a6755
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-05-31 14:22:59 -0300

    Conectando endpoints HTTP

--- projeto-camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ projeto-camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,7 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -13,13 +14,12 @@ public class RotaPedidos {
 			@Override
 			public void configure() throws Exception {
 				from("file:pedidos?delay=5s&noop=true").
-					split().xpath("/pedido/itens/item").
-						log("${body}").
+						split().xpath("/pedido/itens/item").
 						filter().xpath("/item/formato[text()='EBOOK']").
-					marshal().xmljson().
-					log("${body}").
-					setHeader("CamelFileName", simple("${file:name.noext}.json")).
-				to("file:saida");
+						marshal().xmljson().
+						log("${id} - ${body}").
+						setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json")).
+						to("file:saida");
 			}
 		});
 
