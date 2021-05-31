6f23a6ac1618d865f6d81bc02725186716b5fa76
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-05-31 20:12:20 -0300

    Conectando endpoints HTTP

--- projeto-camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ projeto-camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -3,6 +3,7 @@ package br.com.caelum.camel;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -18,8 +19,9 @@ public class RotaPedidos {
 						filter().xpath("/item/formato[text()='EBOOK']").
 						marshal().xmljson().
 						log("${id} - ${body}").
-						setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json")).
-						to("file:saida");
+						setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
+						setHeader(Exchange.HTTP_QUERY, constant("ebookId=ARQ&pedidoId=2451256&clienteId=edgar.b@abc.com")).
+						to("http4://localhost:8080:webservices/ebook/item");
 			}
 		});
 
