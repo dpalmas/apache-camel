7db4f7331e318616da9b737d24fe1430b9591b92
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-05-31 23:21:25 -0300

    Enviando e recebendo mensagens JMS

---
+++ projeto-camel-alura/src/main/java/br/com/caelum/camel/RotaEnviarPedidos.java
@@ -0,0 +1,30 @@
+package br.com.caelum.camel;
+
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class RotaEnviarPedidos {
+
+	public static void main(String[] args) throws Exception {
+
+		CamelContext context = new DefaultCamelContext();
+		context.addComponent("activemq", ActiveMQComponent.activeMQComponent("tcp://localhost:61616/"));
+
+		context.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				from("file:pedidos?noop=true").
+						to("activemq:queue:pedidos");
+			}
+		});
+
+		context.start();
+		Thread.sleep(20000);
+		context.stop();
+	}
+}
