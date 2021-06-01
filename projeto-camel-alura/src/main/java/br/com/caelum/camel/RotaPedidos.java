a9fd11c31275da406992b36359dbebae8b6d7282
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-05-31 21:16:52 -0300

    Conectando endpoints HTTP

--- projeto-camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ projeto-camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -15,12 +15,15 @@ public class RotaPedidos {
 			@Override
 			public void configure() throws Exception {
 				from("file:pedidos?delay=5&noop=true").
+						setProperty("pedidoId", xpath("/pedido/id/text()")).
+						setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
 						split().xpath("/pedido/itens/item").
 						filter().xpath("/item/formato[text()='EBOOK']").
+						setProperty("ebookId", xpath("/item/livro/codigo/text()")).
 						marshal().xmljson().
 						log("${id} - ${body}").
 						setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
-						setHeader(Exchange.HTTP_QUERY, constant("ebookId=ARQ&pedidoId=2451256&clienteId=edgar.b@abc.com")).
+						setHeader(Exchange.HTTP_QUERY, constant("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).
 						to("http4://localhost:8080:webservices/ebook/item");
 			}
 		});
