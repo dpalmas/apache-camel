d8387b8003dacc1620a430d09fae39ee5490f0df
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-11 15:54:19 -0300

    Update RotaPedidos.java

--- projeto-camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ projeto-camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -8,61 +8,41 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
 
-public class RotaPedidos {
+public class RotaPedidos
+{
+    public static void main(String[] args) throws Exception
+    {
+        CamelContext context = new DefaultCamelContext();
+        context.addComponent("activemq", ActiveMQComponent.activeMQComponent("tcp://localhost:61616/"));
 
-	public static void main(String[] args) throws Exception {
+        context.addRoutes(new RouteBuilder()
+        {
+            @Override
+            public void configure() throws Exception
+            {
+                errorHandler(deadLetterChannel("activemq:queue:pedidos.DLQ").logExhaustedMessageHistory(true).maximumRedeliveries(3).redeliveryDelay(2000).onRedelivery(new Processor()
+                {
+                    @Override
+                    public void process(Exchange exchange) throws Exception
+                    {
+                        int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+                        int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+                        System.out.println("Redelivery " + counter + "/" + max);
+                    }
+                }));
 
-		CamelContext context = new DefaultCamelContext();
-		context.addComponent("activemq", ActiveMQComponent.activeMQComponent("tcp://localhost:61616/"));
+                from("activemq:queue:pedidos").routeId("rota-pedidos").to("validator:pedido.xsd").multicast().to("direct:http").to("direct:soap");
 
-		context.addRoutes(new RouteBuilder() {
-			@Override
-			public void configure() throws Exception {
+                from("direct:http").routeId("rota-http").setProperty("pedidoId", xpath("/pedido/id/text()")).setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).split().xpath("/pedido/itens/item").filter().xpath("/item/formato[text()='EBOOK']")
+                        .setProperty("ebookId", xpath("/item/livro/codigo/text()")).marshal().xmljson().log("${id} - ${body}").setHeader(Exchange.HTTP_METHOD, HttpMethods.GET)
+                        .setHeader(Exchange.HTTP_QUERY, constant("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).to("http4://localhost:8080:webservices/ebook/item");
 
-				errorHandler(deadLetterChannel("activemq:queue:pedidos.DLQ").
-						logExhaustedMessageHistory(true).
-						maximumRedeliveries(3).
-						redeliveryDelay(2000).
-						onRedelivery(new Processor() {
-							@Override
-							public void process(Exchange exchange) throws Exception {
-								int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
-								int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
-								System.out.println("Redelivery " + counter + "/" + max);
-							}
-						}));
+                from("direct:soap").routeId("rota-soap").to("xslt:pedido-para-soap.xslt").log("${body}").setHeader(Exchange.CONTENT_TYPE, constant("text/xml")).to("http4://localhost:8080/webservices/financeiro");
+            }
+        });
 
-				from("activemq:queue:pedidos").
-						routeId("rota-pedidos").
-						to("validator:pedido.xsd").
-						multicast().
-						to("direct:http").
-						to("direct:soap");
-
-				from("direct:http").
-						routeId("rota-http").
-						setProperty("pedidoId", xpath("/pedido/id/text()")).
-						setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
-						split().xpath("/pedido/itens/item").
-						filter().xpath("/item/formato[text()='EBOOK']").
-						setProperty("ebookId", xpath("/item/livro/codigo/text()")).
-						marshal().xmljson().
-						log("${id} - ${body}").
-						setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
-						setHeader(Exchange.HTTP_QUERY, constant("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).
-						to("http4://localhost:8080:webservices/ebook/item");
-
-				from("direct:soap").
-						routeId("rota-soap").
-						to("xslt:pedido-para-soap.xslt").
-						log("${body}").
-						setHeader(Exchange.CONTENT_TYPE, constant("text/xml")).
-						to("http4://localhost:8080/webservices/financeiro");
-			}
-		});
-
-		context.start();
-		Thread.sleep(20000);
-		context.stop();
-	}
+        context.start();
+        Thread.sleep(20000);
+        context.stop();
+    }
 }
